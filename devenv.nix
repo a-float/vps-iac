23792c89d66262ccc916ffa9bdf1b47ea6cd2fb1
Mateusz Zalewski <mati852456@gmail.com>
2026-08-14 14:55:40 +0200

    Add tailscale setup and openrouter api key

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,13 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  env.GREET = "devenv";
+  env.ANSIBLE_VAULT_PASSWORD_FILE  = ".ansible_vault_pass";
 
   packages = [ pkgs.git ];
 
   languages.ansible.enable = true;
+
+  scripts.ansible-play.exec = ''
+    ansible-playbook -i inventory/hosts.yml site.yml
+  '';
 }
